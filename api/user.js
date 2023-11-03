ae668b32c212ebeaa55ab016ed5a2ce5fe66cecf
Storm Victor <1451871636@qq.com>
2023-11-03 14:11:10 +0800

    Update user.js

--- api/user.js
+++ api/user.js
@@ -1,8 +1,6 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
 
 const users = ["Bob Smith", "Guillaume Bibeau"];
 
-export default (req: VercelRequest, res: VercelResponse) => {
-  const { id } = req.query;
-  res.status(200).json({ user: users[Number(id)] });
+export default () => {
+  return { user: users[0] };
 };
